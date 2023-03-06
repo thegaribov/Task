a33566a033818bdf7321281f3611abd37019fceb
Mahmood Garibov <mahmood.garibov@gmail.com>
2023-03-06 19:35:44 +0400

    Current calculator completed

---
+++ CurrencyCalculator/CurrencyCalculator/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace CurrencyCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            decimal amountInAzn = 10_000;
+
+            decimal usdRate = 1.7M;
+            decimal euroRate = 1.8092M;
+            decimal rubRate = 0.0225M;
+
+            Console.WriteLine("Mebleg (AZN) : " + amountInAzn);
+            Console.WriteLine();
+            Console.WriteLine("ABŞ dolları (USD) : " + amountInAzn / usdRate);
+            Console.WriteLine("Avro (EUR) : " + amountInAzn / euroRate);
+            Console.WriteLine("Rusiya rublu (RUB) : " + amountInAzn / rubRate);
+        }
+    }
+}
