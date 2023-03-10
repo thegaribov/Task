71b95096cb91d05606b98b128f76c2239159971f
Mahmood Garibov <mahmood.garibov@gmail.com>
2023-03-10 21:05:05 +0400

    Our cargo orde application automated with array

--- CargoOrderCalculation/CargoOrderCalculation/Program.cs
+++ CargoOrderCalculation/CargoOrderCalculation/Program.cs
@@ -6,116 +6,61 @@
         {
             Console.Write("Pls enter brand count : ");
             int brandCount = int.Parse(Console.ReadLine());
-            decimal total = 0;
-            decimal totalWithDiscount = 0;
+
             decimal discountPercentage = 20;
             decimal discountStartNumber = 5;
 
-            while (brandCount >= 1)
+            string[] brandNames = new string[brandCount];
+            decimal[] shoesPrices = new decimal[brandCount];
+            int[] shoesCounts = new int[brandCount];
+            decimal[] shoesTotalPrices = new decimal[brandCount];
+            decimal[] shoesTotalPricesWithDiscount = new decimal[brandCount];
+
+            int index = 0;
+
+            while (index < brandCount)
             {
-                string brandName = "";
-
-                while (true)
-                {
-                    Console.Write("Brand name : ");
-                    brandName = Console.ReadLine();
-
-                    if (brandName.Length > 5 && brandName.Length < 10)
-                    {
-                        break;
-                    }
-                }
-
-
+                Console.Write("Brand name : ");
+                brandNames[index] = Console.ReadLine();
 
                 Console.Write("Count of shoes : ");
-                int countOfShoesFromBrand = int.Parse(Console.ReadLine());
+                shoesCounts[index] = int.Parse(Console.ReadLine());
 
                 Console.Write("Price of shoes : ");
-                decimal priceOfShoesFromBrand = decimal.Parse(Console.ReadLine());
-                decimal totalPriceFromBrand = countOfShoesFromBrand * priceOfShoesFromBrand;
-                decimal totalPriceFromBrandWithDiscount = totalPriceFromBrand;
+                shoesPrices[index] = decimal.Parse(Console.ReadLine());
 
-                Console.WriteLine();
-                Console.WriteLine($"Firma : {brandName}"); //string interpolation
-                Console.WriteLine($"Ayaqqabı sayı : {countOfShoesFromBrand}");
-                Console.WriteLine($"Birinin qiyməti ($) : {priceOfShoesFromBrand}");
-                Console.WriteLine($"Hamısının birlikdə qiyməti ($) : {totalPriceFromBrand}");
-                if (countOfShoesFromBrand > discountStartNumber)
+                shoesTotalPrices[index] = shoesCounts[index] * shoesPrices[index];
+                shoesTotalPricesWithDiscount[index] = shoesTotalPrices[index];
+
+                if (shoesCounts[index] > discountStartNumber)
                 {
-                    totalPriceFromBrandWithDiscount = totalPriceFromBrandWithDiscount - totalPriceFromBrandWithDiscount * discountPercentage / 100;
-                    Console.WriteLine($"Hamısının birlikdə endirimli qiyməti ($) : {totalPriceFromBrandWithDiscount}");
+                    shoesTotalPricesWithDiscount[index] =
+                        shoesTotalPricesWithDiscount[index] - shoesTotalPricesWithDiscount[index] * discountPercentage / 100;
                 }
 
-                Console.WriteLine("############################################################################");
-
-                total = total + totalPriceFromBrand;
-                totalWithDiscount = totalWithDiscount + totalPriceFromBrandWithDiscount;
-                brandCount -= 1;
+                index++;
             }
 
-            Console.WriteLine();
-            Console.WriteLine($"Yekun : {total}");
+            index = 0;
 
-            if (total != totalWithDiscount)
-                Console.WriteLine($"(Yekun) Endirimli : {totalWithDiscount}");
+            while (index < brandCount)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Firma : {brandNames[index]}"); //string interpolation
+                Console.WriteLine($"Ayaqqabı sayı : {shoesCounts[index]}");
+                Console.WriteLine($"Birinin qiyməti ($) : {shoesPrices[index]}");
+                Console.WriteLine($"Hamısının birlikdə qiyməti ($) : {shoesTotalPrices[index]}");
+                Console.WriteLine($"Hamısının birlikdə qiyməti (endirimli) ($) : {shoesTotalPricesWithDiscount[index]}");
 
+                index++;
+            }
 
-
-            //Console.Write("Brand name : ");
-            //string firstBrandName = Console.ReadLine();
-
-            //Console.Write("Count of shoes : ");
-            //int countOfShoesFromFirstBrand = int.Parse(Console.ReadLine());
-
-            //Console.Write("Price of shoes : ");
-            //decimal priceOfShoesFromFirstBrand = decimal.Parse(Console.ReadLine());
-            //decimal totalPriceFromFirstBrand = countOfShoesFromFirstBrand * priceOfShoesFromFirstBrand;
-
-            //Console.Write("Brand name : ");
-            //string secondBrandName = Console.ReadLine();
-
-            //Console.Write("Count of shoes : ");
-            //int countOfShoesFromSecondBrand = int.Parse(Console.ReadLine());
-
-            //Console.Write("Price of shoes : ");
-            //decimal priceOfShoesFromSecondBrand = decimal.Parse(Console.ReadLine());
-            //decimal totalPriceFromSecondBrand = countOfShoesFromSecondBrand * priceOfShoesFromSecondBrand;
-
-
-            //Console.Write("Brand name : ");
-            //string thirdBrandName = Console.ReadLine();
-
-            //Console.Write("Count of shoes : ");
-            //int countOfShoesFromThirdBrand = int.Parse(Console.ReadLine());
-
-            //Console.Write("Price of shoes : ");
-            //decimal priceOfShoesFromThirdBrand = decimal.Parse(Console.ReadLine());
-            //decimal totalPriceFromThirdBrand = countOfShoesFromThirdBrand * priceOfShoesFromThirdBrand;
-
-            //decimal totalPrice = totalPriceFromFirstBrand + totalPriceFromSecondBrand + totalPriceFromThirdBrand;
 
             //Console.WriteLine();
+            //Console.WriteLine($"Yekun : {total}");
 
-            //Console.WriteLine("Firma : " + firstBrandName);
-            //Console.WriteLine("Ayaqqabı sayı : " + countOfShoesFromFirstBrand);
-            //Console.WriteLine("Birinin qiyməti ($) : " + priceOfShoesFromFirstBrand);
-            //Console.WriteLine("Hamısının birlikdə qiyməti ($) : " + totalPriceFromFirstBrand);
-
-            //Console.WriteLine();
-            //Console.WriteLine("Firma : " + secondBrandName);
-            //Console.WriteLine("Ayaqqabı sayı : " + countOfShoesFromSecondBrand);
-            //Console.WriteLine("Birinin qiyməti ($) : " + priceOfShoesFromSecondBrand);
-            //Console.WriteLine("Hamısının birlikdə qiyməti ($) : " + totalPriceFromSecondBrand);
-
-            //Console.WriteLine();
-            //Console.WriteLine("Firma : " + thirdBrandName);
-            //Console.WriteLine("Ayaqqabı sayı : " + countOfShoesFromThirdBrand);
-            //Console.WriteLine("Birinin qiyməti ($) : " + priceOfShoesFromThirdBrand);
-            //Console.WriteLine("Hamısının birlikdə qiyməti ($) : " + totalPriceFromThirdBrand);
-
-            //Console.WriteLine();
-            //Console.WriteLine("Total :" + totalPrice);
+            //if (total != totalWithDiscount)
+            //    Console.WriteLine($"(Yekun) Endirimli : {totalWithDiscount}");
         }
     }
 }
